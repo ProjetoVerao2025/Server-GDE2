3561442ddbd0072252d794306e95652dc01e3cb8
Victor Marques <vhmar2005@gmail.com>
2026-01-26 20:50:41 -0300

    Implemented str methods

--- GDE2/Academic/models.py
+++ GDE2/Academic/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.contrib.auth.hashers import make_password
+
 # Create your models here.
 
 class Attendance(models.Model):
@@ -8,23 +10,43 @@ class Attendance(models.Model):
         absent = 2
         excused = 3
 
-    class_id = models.IntegerField()
-    student_id = models.IntegerField()
+    class_id = models.OneToOneField(
+        "Academic.Class",
+        on_delete = models.CASCADE
+    )
+
+    student_id = models.OneToOneField(
+        "Academic.Student",
+        on_delete = models.CASCADE
+    )
+
     date = models.DateTimeField()
     status = models.IntegerField(choices = AttendanceStatus)
 
-class Class(models.Model):
+class Class(models.Model):  # sala específica (F159 Z)
+    class ClassOffering(models.IntegerChoices): # Período em que a matéria é oferecida
+        odd = 1
+        even = 2
+        sp = 3
+
     id = models.IntegerField(primary_key = True)
-    class_code = models.IntegerField()
+    course_code = models.OneToOneField(
+        "Academic.Course",
+        on_delete = models.CASCADE
+    )
     letter = models.CharField()
+    year_offered = models.IntegerField()
+    period_offered = models.IntegerChoices(choices = ClassOffering)
 
+    def __str__(self):
+        return f"{self.class_code}({self.letter})"
 
-class Course(models.Model):
-    class CourseLevel(models.IntegerChoices):
+class Course(models.Model): # matéria que você paga
+    class CourseLevel(models.IntegerChoices): # nível de graduação
         Undergraduate = 1
         Graduate = 2
 
-    class CourseOffering(models.IntegerChoices):
+    class CourseOffering(models.IntegerChoices): # Período em que a matéria é oferecida
         odd = 1
         even = 2
         both = 3
@@ -33,32 +55,56 @@ class Course(models.Model):
     id = models.IntegerField(primary_key = True)
     code = models.CharField(max_length = 5)
     name = models.CharField(max_length = 100)
-    CourseLevel = models.IntegerField(choices = CourseLevel) 
+    courseLevel = models.IntegerField(choices = CourseLevel) 
     department = models.IntegerField()
     credits = models.IntegerField()
     offered = models.IntegerField(choices = CourseOffering) 
-    syllabus = models.CharField(max_length = 1000) #** n sei quanto exatamente colocar
+    syllabus = models.CharField(max_length = 1000) # ementa
+
+    def __str__(self):
+        return f"{self.code}"
  
-class CourseRequirement(models.Model):
-    course_code = models.IntegerField()
-    requirement_code = models.IntegerField()
+class CourseRequirement(models.Model): # prérequisitos (MC202 precisa de MC102)
+    course_code = models.OneToOneField( # essa matéria depende
+        "Academic.Course",
+        on_delete = models.CASCADE
+    )
+    requirement_code = models.ForeignKey(
+        "Academic.Requirement",
+        on_delete = models.CASCADE
+    ) # dessa
 
 class Department(models.Model):
     id = models.IntegerField(primary_key = True)
     name = models.CharField(max_length = 100)
     acronym = models.CharField(max_length = 10)
 
+    def __str__(self):
+        return f"{self.acronym}"
+
 class Enrollment(models.Model):
-    students = models.IntegerField()
-    class_id = models.IntegerField()
+    student_id = models.OneToOneField(
+        "Academic.Student",
+        on_delete = models.CASCADE
+    )
+    class_id = models.OneToOneField(
+        "Academic.Class",
+        on_delete = models.CASCADE
+    )
 
 class Professor(models.Model):
     id = models.IntegerField(primary_key = True)
     name = models.CharField(max_length = 100)
 
-class Program(models.Model):
+    def __str__(self):
+        return f"{self.name}"
+
+class Program(models.Model): # Curso que você cursa (CC == 42)
     id = models.IntegerField(primary_key = True)
     name = models.CharField(max_length = 100)
+    
+    def __str__(self):
+        return f"{self.name}"
 
 class Student(models.Model):
     class StudentLevel(models.IntegerChoices):
@@ -68,8 +114,20 @@ class Student(models.Model):
         Exchange = 4
         VisitingStudent = 5
     
-    ra = models.IntegerField()
+    ra = models.IntegerField(primary_key = True)
     name = models.CharField(max_length = 100)
-    program_code = models.IntegerField()
+    program_code = models.OneToOneField(
+        "Academic.Program",
+        on_delete = models.CASCADE
+    )
     level = models.IntegerField(choices = StudentLevel)
+    email = models.CharField(max_length = 255, default = None)
+    password = models.CharField(max_length = 255, default = None)
 
+    def __str__(self):
+        return f"{self.ra:06d}"
+
+    def save(self):
+        if not self.password.startswith('pbkdf2_sha256$'):
+            self.password = make_password(self.password)
+        super().save()
